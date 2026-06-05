140474999f59126487081b003e9118f1d6501a73
zagros1717 <60982885+zagros1717@users.noreply.github.com>
2026-06-05 12:40:53 +0200

    Add context24 package

---
+++ src/context24/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import annotations
+
+from src.context24.registry import METRICS, MetricDefinition
+from src.context24.scoring import score_table
+
+__all__ = ["METRICS", "MetricDefinition", "score_table"]
